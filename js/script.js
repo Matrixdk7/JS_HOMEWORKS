4082892abc5d318d6726677af3f5d2734e858424
Matrixdk7 <matrixdk7@gmail.com>
2025-11-20 04:58:45 +0200

    hw_1-2 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const userName = prompt('What is your name?');
+const welcomeMes = confirm(`Hello, ${userName}! Would you like to receive welcome messages in the future?`);
+if (welcomeMes) {
+    alert("You will continue to receive welcome messages!");
+} else {
+    alert("You will no longer receive welcome messages.");
+}
--- readme.md
+++ readme.md
@@ -1,1 +1,9 @@
-### Homeworks
+### Homework #1-2
+
+A simple page that, upon loading, asks the user to enter their name and confirm whether they want to see a greeting. Based on the response, it outputs an `alert()`.
+
+#### Used:
+- `alert`
+- `confirm`
+- `prompt`
+- `if`
