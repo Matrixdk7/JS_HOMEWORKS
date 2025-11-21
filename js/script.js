773a3a6695d4e65781bd372d7069f752f5e414df
Matrixdk7 <matrixdk7@gmail.com>
2025-11-21 04:33:46 +0200

    Remove "else"

--- js/script.js
+++ js/script.js
@@ -4,6 +4,4 @@ const userName = prompt('What is your name?');
 const welcomeMes = confirm(`Hello, ${userName}! Would you like to receive welcome messages in the future?`);
 if (welcomeMes) {
     alert("You will continue to receive welcome messages!");
-} else {
-    alert("You will no longer receive welcome messages.");
 }
