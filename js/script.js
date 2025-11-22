0e1ea7ad51f8720e539bbb0fe9b70629bcefaeda
Matrixdk7 <matrixdk7@gmail.com>
2025-11-22 22:38:34 +0200

    hw 2-1 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const price = +prompt(`Enter the price of the product`);
+if (isNaN(price) || price < 0) {
+    alert(`Input error`)
+} else {
+    const discount = +prompt(`Enter the discount amount as a percentage`);
+    if (isNaN(discount) || discount < 0) {
+        alert(`Input error`)
+    } else {
+        const discPrice = (price - (price * discount/100));
+        alert(`Starting price: ${price}$, discount: ${discount}%. Discounted price: ${discPrice}$.`);
+    }
+}
--- readme.md
+++ readme.md
@@ -1,1 +1,8 @@
-### Homeworks
+### Homework #2-1
+
+A simple in-store discount calculator.
+
+- Converts entered values into numbers.
+- Verifies that the offensive value is correct (not NaN, not less than 0).
+- Calculate the price based on the discount.
+- Enter via alert on the website: "Earth price: 100 dollars, discount: 15%. Price with discount: 85 dollars."
