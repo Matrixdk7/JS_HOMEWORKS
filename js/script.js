d15563b9b5a160e0e43fa676f5b89d917c44ee20
Matrixdk7 <matrixdk7@gmail.com>
2025-12-04 02:54:29 +0200

    hw 5-2 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,43 @@
 'use strict';
+
+// Task 1
+for(let i=20; i<=30;i += 0.5){
+    console.log(i);
+}
+
+// Task 2
+const uah = 27;
+for (let i = 10; i <= 100; i += 10){
+    let a = i*uah;
+    console.log(`${i} dollars = ${a} uah`);
+}
+
+//Task 3
+const num = +prompt('Enter a number');
+for (let i = 1; i <= 100; i += 1 ){
+    if (i * i <= num) {
+        console.log(i);
+    }
+}
+
+//Task 4
+const num2 = +prompt('Enter a number');
+if (num2 <= 1) {
+    console.log('Not simple');
+} else {
+    let numVerify = true;
+    for (let i = 2; i < num2; i += 1) {
+        if (num2 % i === 0 ) {
+            numVerify = false;
+            break;
+        }
+    }
+    console.log(numVerify ? 'Simple' : 'Not simple');
+}
+
+// Task 5
+let num3 = +prompt('Enter a number');
+while (num3 % 3 === 0) {
+    num3 = num3 / 3;
+}
+console.log(num3 === 1 ? "Yes" : "No");
--- readme.md
+++ readme.md
@@ -1,1 +1,9 @@
-### Homeworks
+### Homework #5-2
+
+Write loops that do the following:
+
+- Print the numbers from 20 to 30 with a gap, using a step of 0.5 (20 20.5 21 21.5….).
+- One dollar costs 27 hryvnias. Print the data with the calculation of the cost of 10, 20, 30... 100 dollars.
+- Given an integer. Print all integers from 1 to 100 whose square does not exceed the number N.
+- Given an integer. Find out if it is prime (a prime number is a number greater than 1 that has no other divisors than 1 and itself).
+- Given a certain number. Determine whether this number can be obtained by raising the number 3 to a certain power. (For example, the numbers 9, 81 can be obtained, but 13 cannot).
