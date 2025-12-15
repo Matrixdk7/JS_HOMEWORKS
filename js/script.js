07724152c96175f44ecf5fc998ba55c7a6fb0a06
Matrixdk7 <matrixdk7@gmail.com>
2025-12-15 12:15:09 +0200

    hw 8-2 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,63 @@
 'use strict';
+
+// Task 1
+const positiveNumbers = arr.filter(num => num > 0);
+
+const sum = positiveNumbers.reduce((acc, num) => acc + num, 0);
+const count = positiveNumbers.length;
+
+console.log(sum);
+console.log(count);
+
+// Task 2
+const min = arr.reduce((acc, num, index) => {
+    if (num < acc.value) {
+        acc.value = num;
+        acc.index = index;
+    }
+    return acc;
+}, { value: Infinity, index: -1 });
+
+console.log(min.value);
+console.log(min.index);
+
+// Task 3
+const max = arr.reduce((acc, num, index) => {
+    if (num > acc.value) {
+        acc.value = num;
+        acc.index = index;
+    }
+    return acc;
+}, { value: -Infinity, index: -1 });
+
+console.log(max.value);
+console.log(max.index);
+
+// Task 4
+const negativeCount = arr.filter(num => num < 0).length;
+console.log(negativeCount);
+
+//Task 5
+const oddPositiveCount = arr.filter(num => num > 0 && num % 2 !== 0).length;
+console.log(oddPositiveCount);
+
+//Task 6
+const evenPositiveCount = arr.filter(num => num > 0 && num % 2 === 0).length;
+console.log(evenPositiveCount);
+
+//Task 7
+const evenPositiveSum = arr.filter(num => num > 0 && num % 2 === 0).reduce((acc, num) => acc + num, 0);
+console.log(evenPositiveSum);
+
+//Task 8
+const oddPositiveSum = arr.filter(num => num > 0 && num % 2 !== 0).reduce((acc, num) => acc + num, 0);
+console.log(oddPositiveSum);
+
+//Task 9
+const positiveMulti = arr.filter(num => num > 0).reduce((acc, num) => acc * num, 1);
+console.log(positiveMulti);
+
+//Task 10
+const maxElement = Math.max(...arr);
+const result = arr.map(num => num === maxElement ? num : 0);
+console.log(result);
--- readme.md
+++ readme.md
@@ -1,1 +1,14 @@
-### Homeworks
+### Homework #8-2
+
+Given an `array [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47]`
+
+1. Find the sum and number of positive elements.
+2. Find the minimum element of the array and its ordinal number.
+3. Find the maximum element of the array and its ordinal number.
+4. Determine the number of negative elements.
+5. Find the number of odd positive elements.
+6. Find the number of even positive elements.
+7. Find the sum of even positive elements.
+8. Find the sum of odd positive elements.
+9. Find the product of positive elements.
+10. Find the largest among the elements of the array, set the rest to zero.
