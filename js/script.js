e7f5eee9a734b8f4cd427432365d404765cc9713
Matrixdk7 <matrixdk7@gmail.com>
2025-12-25 14:11:55 +0200

    hw 11-2 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+/**
+ * Flattens a multidimensional array into a new one-dimensional array.
+ *
+ * @param {Array} arr The array to flatten. Can contain nested arrays of any depth.
+ * @returns {Array} A new flattened array.
+ * @throws {Error} Throws an error if more than one argument is passed to the function.
+ */
+function myFlat(arr) {
+    if (arguments.length > 1) throw new Error('Function accepts only 1 argument, too much arguments provided');
+
+    const result = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (Array.isArray(arr[i])) {
+            const arrResult = myFlat(arr[i]);
+            result.push(...arrResult);
+        } else {
+            result.push(arr[i]);
+        }
+    }
+
+    return result;
+}
+
+console.log(myFlat([1, [2, [3, 4]], 5]));
--- readme.md
+++ readme.md
@@ -1,1 +1,12 @@
-### Homeworks
+### Homework #11-2
+
+Your function should repeat the basic functionality of the `flat` method, but with some improvements.
+
+- The function accepts only 1 argument - an array,
+- The function returns only a one-dimensional array
+- If the user passes more than one argument, throw an error to the console
+
+- If a multidimensional array is passed to the function, then it must return a new one-dimensional array.
+- If a one-dimensional array is passed to the function, then it must return a new array that is a complete copy of the one passed.
+
+P.S. The use of the `flat` method is prohibited
