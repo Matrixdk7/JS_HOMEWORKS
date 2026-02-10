ed59cca814ebfc0d6e48979a3988f10a4107cf4c
Matrixdk7 <matrixdk7@gmail.com>
2026-02-10 15:53:42 +0200

    hw 19 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,85 @@
 'use strict';
+
+// Object
+const user = {
+    lockProfile() {
+        Object.seal(this)
+    },
+
+    freezeProfile() {
+        Object.freeze(this)
+    }
+}
+
+// Services properties
+Object.defineProperties(user, {
+    _firstName: {
+        value: null,
+        writable: true,
+        enumerable: false,
+    },
+    _lastName: {
+        value: null,
+        writable: true,
+        enumerable: false,
+    },
+    createdAt: {
+        value: new Date(),
+        writable: false,
+        enumerable: false,
+        configurable: false
+}
+});
+
+// Public properties
+Object.defineProperties(user, {
+    fullName: {
+        enumerable: true,
+        configurable: false,
+
+        get() {
+            return this._firstName + ' ' + this._lastName;
+        },
+
+        set(value) {
+            const fullName = user.fullName;
+            const parts = value.trim().split(' ');
+            console.log(parts);
+
+            if (parts.length !== 2) {
+                throw new Error("Full name must contain exactly two words");
+            }
+
+            if (parts[0].length < 2 || parts[1].length < 2) {
+                throw new Error("Each word must be at least 2 characters");
+            }
+            this._firstName = parts[0];
+            this._lastName = parts[1];
+        }
+    }
+});
+
+// Example 1
+user.fullName = "John Doe";
+console.log(user);
+console.log(Object.getOwnPropertyDescriptors(user));
+
+// Seal
+user.lockProfile();
+console.log(Object.isSealed(user));
+user.newString = 777;
+console.log(user.newString);
+
+// Freeze
+// user.freezeProfile();
+// console.log(Object.isFrozen(user));
+// user.fullName = "Alice Cooper";
+// console.log(user.fullName);
+
+// Example 2
+// user.createdAt = new Date(2000, 0, 1);
+// console.log(user.createdAt);
+
+// Example 3
+// delete user.fullName;
+// console.log(user.fullName);
--- readme.md
+++ readme.md
@@ -1,1 +1,30 @@
-### Homeworks
+### Homework #19
+
+Завдання
+
+1. Створіть об’єкт user із полями:
+- _firstName (службове поле)
+- _lastName (службове поле)
+- createdAt (дата створення, тільки для читання)
+2. Додайте accessor-властивість fullName:
+- getter повертає "First Last"
+- setter приймає рядок "First Last" і:
+- перевіряє, що це string
+- перевіряє, що є 2 слова
+- кожне слово мінімум 2 символи
+- якщо невалідно — кидає Error
+3. Через дескриптори налаштуйте:
+- _firstName, _lastName: enumerable: false
+- createdAt: writable: false, configurable: false
+- fullName: configurable: false, enumerable: true
+4. Додайте метод lockProfile():
+- після виклику об’єкт має стати sealed
+- перевірте через Object.isSealed(user)
+5. Виведіть у консоль:
+- дескриптори всіх полів (Object.getOwnPropertyDescriptors)
+- результат спроби:
+- додати нове поле після lockProfile
+- змінити createdAt
+- видалити fullName
+
+Зробіть версію lockHard(), яка використовує Object.freeze(user), і порівняйте поведінку із seal.
